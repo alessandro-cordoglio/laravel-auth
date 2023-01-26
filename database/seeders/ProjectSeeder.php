b1a256b9261b13644a86c6edd187eae3451c25df
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-26 17:52:52 +0100

    setup CRUD, aggiunti dati fake al database

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Project;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i= 0; $i < 5 ; $i++) { 
+            $new_project= new Project;
+            $new_project->title = $faker->sentence(3);
+            $new_project->description = $faker->text(150);
+            $new_project->slug= Str::slug($new_project->title,'-');;
+            $new_project->client= $faker->name();
+            $new_project->save();
+        }   
+    }
+}
