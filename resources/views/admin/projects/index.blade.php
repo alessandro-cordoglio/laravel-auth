01d64eff7032959cbc7bdf8bbfb3bb4978d4c67f
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-26 20:08:25 +0100

    implementata index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
     <h1>Progetti</h1>    
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Titolo</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Cliente</th>
+            <th scope="col">placeholder</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+            @foreach ($projects as $project)
+            <tr>
+                <td>{{$project->title}}</td>
+                <td>{{$project->slug}}</td>
+                <td>{{$project->client}}</td>
+            </tr>
+            @endforeach
+          
+        </tbody>
+      </table>
 @endsection
